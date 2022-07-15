7a5cdd4a2fc45fad1e8bf726abe00913e5518422
daryle-tan <daryle.tan@gmail.com>
2022-07-15 17:42:52 -0500

    pet-shop-express-bonus1

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { readPetsFile } from './shared.js';
-import fs from "fs/promises"
+import { writeFile } from 'fs/promises';
+import fs from "fs/promises";
 
 const app = express();
 const PORT = 3000;
@@ -8,23 +9,38 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/pets", (req, res) => {
-    readPetsFile().then(str => {
-       res.send(str);
+    readPetsFile().then(obj => {
+       res.send(obj);
     })
 });
 
 app.get("/pets/:id/", (req, res) => {
-    readPetsFile().then(str => {
-        if (str[req.params.id]) {
-            res.send(str[req.params.id]);         
+    readPetsFile().then(obj => {
+        if (obj[req.params.id]) {
+            res.send(obj[req.params.id])
         } else {
-            res.status(404).send('Not Found')
+            res.sendStatus(404)
         }
     })
 });
 
+app.post("/pets", (req, res) => {
+    const body = req.body;
+    readPetsFile().then(obj => {
+        const pets = obj;
+        pets.push(body);
+        res.send(pets)
 
+        writeFile("pets.json", JSON.stringify(pets)).then((err) => {
+            if (err) {
+                res.send(err)
+            } else {
+                res.send(pets)
+            }
+        })
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
-} )
+})
